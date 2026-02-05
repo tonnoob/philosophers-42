e6394c9f9929c1100ef38aad075655870adea1a4
Otton Sousa De Santana <osousa-d@c1r1p2.42sp.org.br>
2026-02-05 12:06:33 -0300

    init validation args

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -1,6 +1,9 @@
 #ifndef PHILO_H
 # define PHILO_H
 
+int	ft_atoi(const char *str);
+int	ft_isdigit(int c);
+int	check_overflow(char *arr);
 
 
 
--- philo/src/main.c
+++ philo/src/main.c
@@ -1,1 +1,44 @@
-#include 
+#include "philo.h"
+
+// number_of_philosophers INT
+// time_to_die LONG
+// time_to_eat LONG
+// time_to_sleep LONG
+// [number_of_times_eargch_philosopher_must_eat] - não é obrigatorio o input, mas é mandatorio
+
+// CRIAR STRUCTS
+int	valid_input(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	if (argc <= 5 && argc >= 6)
+		return (0);
+	i = 0;
+	while (argv[i][j])
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (!ft_isdigit(argv[i][j]))
+				return (0);
+			j++;
+		}
+		if (!check_overflow(argv[i]))
+			return (0);
+		i++;
+	}
+			
+	//chamar atol em loop
+	if ()
+}
+
+
+int	main(int argc, char **argv)
+{
+	if (!valid_input(argc, argv))
+		return (1);
+	
+	
+	return (0);
+}
